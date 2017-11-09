86ab0ac667b1654fa271903e4473617c4fa575bd
Scott Bellware <sbellware@gmail.com>
2017-11-09 14:04:58 -0600

    Installing gems for this project requires no recognition of environment or posture

--- install-gems.sh
+++ install-gems.sh
@@ -2,22 +2,11 @@
 
 set -e
 
-if [ -z ${POSTURE+x} ]; then
-  echo "(POSTURE is not set. Using \"operational\" by default.)"
-  posture="operational"
-else
-  posture=$POSTURE
-fi
-
 echo
-echo "Installing gems locally (posture: $posture)"
+echo "Installing gems to ./gems"
 echo '= = ='
 
 cmd="bundle install --standalone --path=./gems"
-
-if [ operational == "$posture" ]; then
-  cmd="$cmd --without=development"
-fi
 
 echo $cmd
 ($cmd)
